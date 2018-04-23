f97127a6bfee97c0f027bfc15cfcdb065e91d409
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to hide/show a LayoutGroup
+
+
+<p>Get the Items collection of the RootGroup, and remove/add the necessary group</p>
+
+<br/>
+
+
---
+++ VB/CreateLayoutGroups/Window1.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace CreateLayoutGroups
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			RootGroup.Items.Remove(LayoutGroup3)
+		End Sub
+
+		Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			RootGroup.Items.Add(LayoutGroup3)
+		End Sub
+	End Class
+End Namespace
